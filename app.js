8d3a17b597b223f285a1b6d75fd600f9d81aeedd
sakhura <sabina.romero@gmail.com>
2026-02-19 20:48:03 -0300

    sacar error de redaccion

---
+++ app.js
@@ -0,0 +1,65 @@
+// Version usando .then() y .catch()
+
+function obtenerUsuariosConThen(){
+    //limpiar el contenedor antes de cargar
+    document.getElementById("contenedor").innerHTML = "Cargando...";
+    document.getElementById("mensaje-error").textContent = "";
+
+    fetch("https://jsonplaceholder.typicode.com/users") //1. hacemos la peticion
+        .then(function(respuesta){                      //2. recibimos la respuesta
+            if(!respuesta.ok){
+                throw new Error("Error en la petición: " + respuesta.status);
+            }
+            return respuesta.json();                    //3.convertimos a json
+        })
+        .then(function(usuarios){                       //4. ya tenemos los datos
+            mostrarUsuarios(usuarios);                  //5. mostramos los usuarios
+        })
+        .catch(function(error){                         //6. capturamos cualquier error
+            document.getElementById("mensaje-error").textContent = "X" + error.message;
+        });
+
+}
+
+// fx que construya las tarjetas en el DOM
+
+function mostrarUsuarios(usuarios){
+    const contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML= "";
+
+    usuarios.foreach(function(usuario){
+        //se creara una tarjeta por usuario
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <h3>${usuario.name}</h3>
+            <p> Email: ${usuario.email} </p>
+            <p> Ciudad: ${usuario.address.city} </p>
+            <p> Web: ${usuario.website} </p>
+        `;
+        contenedor.appendChild(card);
+    });
+}
+
+//version async/await 
+
+async function obtenerUsuariosConAsync() {
+    document.getElementById("contenedor").innerHTML= "Cargando ...";
+    document.getElementById("mensaje-error").textContent = "";
+
+    try{
+        const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        if(!respuesta.ok){
+            throw new Error("Error en la petición: " + respuesta.status);
+        }
+
+        const usuarios = await respuesta.json();
+        mostrarUsuarios(usuarios);
+    }catch (error){
+        document.getElementById("mensaje-error").textContent = "X" + error.message;
+
+    }
+
+    
+}
